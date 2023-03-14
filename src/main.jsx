f8244651bb9e99d9f709d808d63a64aa7f0e1f22
rlagosg <rlagosg@yahoo.com>
2023-03-14 09:45:49 -0600

    update

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { FormWithCustomHook } from './components/FormWithCustomHook'
+
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+    <FormWithCustomHook />
+  // </React.StrictMode>,
+)
